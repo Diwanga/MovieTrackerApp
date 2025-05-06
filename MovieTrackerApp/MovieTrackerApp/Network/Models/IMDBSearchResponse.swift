bf793d49af745e8ef25ee3e8798e94ea82715ff9
diwangaamasith <diwangaamasith@gmail.com>
2025-05-06 20:57:45 +0530

    added network  modals

---
+++ MovieTrackerApp/MovieTrackerApp/Network/Models/IMDBSearchResponse.swift
@@ -0,0 +1,18 @@
+//
+//  IMDBSearchResponse.swift
+//  MovieTrackerApp
+//
+//  Created by Diwanga Amasith on 2025-05-03.
+//
+
+
+import Foundation
+import Combine
+
+
+struct IMDBSearchResponse: Codable {
+    let rows: Int
+    let numFound: Int
+    let results: [MovieResponse]
+    let nextCursorMark: String?
+}
